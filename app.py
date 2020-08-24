15840ed3cb9b0abaa854347b02bbf33c37c322d9
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-08-24 17:30:53 -0400

    update surfs up folder and building app.

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
-# Import Flask dependency.
-from flask import Flask
+################################
+# Set Up the Flask Weather App
+###############################
+# Import dependencies.
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+# Import dependencies for SQLAlchemy.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+######################
+# Set Up the Database
+######################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect exisiting database into a new model
+Base = automap_base()
+
+#reflect the tables
+Base.prepare(engine, reflect=True)
+
+# keys
+Base.classes.keys()
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+# Create session from python to the DB
+session = Session(engine)
+
+###############
+# Set Up Flask
+###############
 # Create a New Flask App Instance.
 app = Flask(__name__)
 # Note: the "name" variable inside of the flask() function denotes
@@ -13,11 +51,24 @@ app = Flask(__name__)
 # Create Flask Routes.
 # 1. Define the starting point, known as the "root".
 @app.route('/')
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:<br/>
+    /api/v1.0/precipitation<br/>
+    /api/v1.0/stations<br/>
+    /api/v1.0/tobs<br/>
+    /api/v1.0/temp/start/end<br/>
+    ''')
 
-# 2. Practice: create a function called hello_world().
-@app.route('/')
-def hello_world():
-    return 'Hello World'
-
-# 3. Run a Flask App (using terminal).
-
+# 2. Define precipitation route.
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
